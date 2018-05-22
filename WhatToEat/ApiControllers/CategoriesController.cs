0de27c40d2038396a388446093298439e9c3268d
zespoloweprogramowanie@gmail.com <zespoloweprogramowanie@gmail.com>
2018-05-22 20:07:20 +0200

    komentarzee w połowie

--- WhatToEat/ApiControllers/CategoriesController.cs
+++ WhatToEat/ApiControllers/CategoriesController.cs
@@ -42,25 +42,5 @@ namespace WhatToEat.ApiControllers
             });
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
     }
 }
